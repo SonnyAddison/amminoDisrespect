e5b06029414f98286915cfa7059565bbc508576a
SonnyAddison <101370278+SonnyAddison@users.noreply.github.com>
2022-06-13 20:41:23 -0700

    Updated JSS for Workout

--- assets/swole.js
+++ assets/swole.js
@@ -1,6 +1,6 @@
 var startSearchEl = document.getElementById('startBtn');
 var startEl = document.getElementById('start');
-var workOutEl = document.getElementById('workout');
+var workOutEl = document.getElementById('workouts');
 
 
 const apiKey = '7ee914cf916f77eb82f1f2769585c24eb087b281';
@@ -17,15 +17,38 @@ function start () {
     return resp.json();
   })
   .then(function (data) {
-    console.log(data);
-  });
-  displaySelectTemplate ();
+    for (var i = 0; i < 16; i ++) {
+    console.log(data.results[i].name);
+    if (!data.results[i].name) {
+      return null;
+    } 
+    var workOutNames = data.results[i].name;
+
+    var workOutNameEl = document.createElement('button');
+    workOutNameEl.classList = 'listItem flexRow justifySpaceBetween alignCenter listBtn';
+
+    var titleEl = document.createElement('span');
+    titleEl.textContent = workOutNames;
+
+    workOutNameEl.appendChild(titleEl);
+    workOutEl.appendChild(workOutNameEl);
+
+    startSearchEl.classList.add('hide');
+
+    }
+  }); 
 }
 
-function displaySelectTemplate (data) { 
+function displayWorkOut () {
+  
+}
+
+
+function displaySelectTemplate () { 
   var templateContainerEl = document.createElement('div');
   var templateHeaderEl = document.createElement('h2');
   var templateBox = document.createElement('div');
+  
 
   templateHeaderEl.textContent ='Select Workout';
 
