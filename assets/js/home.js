8374566654aa4b02278f57f3fd2ce65bd7f62502
SonnyAddison <101370278+SonnyAddison@users.noreply.github.com>
2022-06-15 00:24:29 -0700

    API Second Call is console logging, need to work on getting it to display

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,7 +2,7 @@ var startSearchEl = document.getElementById('startBtn');
 var startEl = document.getElementById('start');
 var bodyEl = document.getElementById('body');
 var instructionEl = document.getElementById('instructions');
-var absEl = document.getElementById('abs');
+
 
 
 headers = {'Authorization': 'Token 7ee914cf916f77eb82f1f2769585c24eb087b281'}
@@ -20,18 +20,23 @@ var start = function () {
                 for (var i = 0; i < 7; i ++) {              
                 var bodyNames = data.results[i].name;
                 
-                var bodyNameEl = document.createElement('a');
-                bodyNameEl.classList = 'listItem flexRow justifySpaceBetween alignCenter listBtn';                   
+                var bodyNameEl = document.createElement('button');
+                bodyNameEl.setAttribute('id', bodyNames);
+                bodyNameEl.classList = 'listItem flexRow justifySpaceBetween alignCenter listBtn'; 
+                bodyNameEl.setAttribute('href', './swole.html?');                              
                 
                 var titleEl = document.createElement('span');
-                titleEl.textContent = bodyNames;
-                titleEl.setAttribute('id', bodyNames);
-                
+                titleEl.textContent = bodyNames;            
                 bodyNameEl.appendChild(titleEl);
                 bodyEl.appendChild(bodyNameEl);
+
+                var absEl = document.getElementById('Abs')
+            
+                absEl.addEventListener('click', getAbs)
                 
-                startSearchEl.classList.add('hide');      
-                }
+
+                startSearchEl.classList.add('hide'); 
+                }   
             })
         })
 }
@@ -51,7 +56,6 @@ var getAbs = function () {
 
             var discriptionsEl = document.createElement('div');
             discriptionsEl.classList ='cont2 center 3';
-            discriptionsEl.setAttribute('href', '.swole.html');
                         
             var titleEl = document.createElement('h1')
             titleEl.textContent = workOutNames;
@@ -61,27 +65,25 @@ var getAbs = function () {
             readOutEl.classList = 'p'
             readOutEl.textContent = descriptions;                
             }
+            
         })
     })
+  }
+   
+/*git var getArms = function () {
+  var apiUrl = 'https://wger.de/api/v2/exercise/?category=8&language=2&limit=1&offset=1';
 
+    fetch(apiUrl, {headers}) 
+      .then(function (resp) {
+        console.log(resp);
+      return resp.json()
+        .then(function (data) {
+          console.log(data)
+        })
+      })
   }
   
-  /*function displayArms () {
-  
-    fetch('https://wger.de/api/v2/exercise/?category=8&language=2&limit=1&offset=1', {
-      headers: {
-        'Authorization': 'Token 7ee914cf916f77eb82f1f2769585c24eb087b281'
-      }
-    })
-    .then(function (resp) {
-      return resp.json();
-    })
-    .then(function (data) { 
-      console.log(data);
-    });
-  }
-  
-  function displayBack () {
+  /*function displayBack () {
     fetch('https://wger.de/api/v2/exercise/?category=12&language=2&limit=1&offset=1', {
       headers: {
         'Authorization': 'Token 7ee914cf916f77eb82f1f2769585c24eb087b281'
@@ -151,6 +153,7 @@ var getAbs = function () {
     });
   }*/
 
-startSearchEl.addEventListener('click', getAbs);
+  startSearchEl.addEventListener('click', start);
+
 
 
